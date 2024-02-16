1dc42a7f526cd8a5c997fc8c27047aa99fb06265
Neto test <test@neto.com>
2024-02-16 16:04:08 +0400

    Initial commit

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -1,13 +1,41 @@
 package ru.netology;
 
-
+//import io.github.bonigarsia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardTest {
+    private WebDriver driver;
+    @BeforeAll
+    public static void setupAll() {
+       // WebDriverManager.chromedriver().setup();
+    }
+    @BeforeEach
+     public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void afterEach(){
+        driver.quit();
+        driver = null;
+    }
+
+
     @Test
     void cardTest() {
         open("http://localhost:9999");
